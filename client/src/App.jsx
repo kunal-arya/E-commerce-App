9f4f39afe757d01312c4428c42246c5b5572e222
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2023-01-24 18:47:31 +0530

    navbar completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,32 @@
-import {Routes,Route} from "react-router-dom"
+import {Routes,Route, Outlet} from "react-router-dom"
+
+import Home from "./pages/Home/Home"
+import Product from "./pages/Product/Product"
+import Products from "./pages/Products/Products"
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
+import "./App.scss"
+
+function Layout() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 export default function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<span>Home</span>} />
-        <Route path="/products/:id" element={<span>Category</span>} />
-        <Route path="/product/:id" element={<span>Product</span>} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:id" element={<Products />} />
+          <Route path="/product/:id" element={<Product />} />
+        </Route>
       </Routes>
     </>
   )
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import SearchIcon from "@mui/icons-material/Search"
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined"
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined"
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined"
+import {Link} from "react-router-dom"
+import "./Navbar.scss"
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+                <div className="left">
+                    <div className="item">
+                        <img src="/images/in.png" alt="" />
+                        <KeyboardArrowDownIcon />
+                    </div>
+                    <div className="item">
+                        <span>INR</span>
+                        <KeyboardArrowDownIcon />
+                    </div>
+                    <div className="item">
+                        <Link className='link' to="/products/1">Women</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to="/products/2">Men</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to="/products/3">Children</Link>
+                    </div>
+                </div>
+                <div className="center">
+                    <Link className='link' to="/">Urban Oasis</Link>
+                </div>
+                <div className="right">
+                    <div className="item">
+                        <Link className='link' to="/">Home</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to="/">About</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to="/">Contact Us</Link>
+                    </div>
+                    <div className="item">
+                        <Link className='link' to="/">Stores</Link>
+                    </div>
+                    <div className="icons">
+                        <SearchIcon />
+                        <PersonOutlineOutlinedIcon />
+                        <FavoriteBorderOutlinedIcon />
+                        <div className="cartIcon">
+                            <ShoppingCartOutlinedIcon />
+                            <span>0</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
