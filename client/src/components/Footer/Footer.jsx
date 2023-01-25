bef653c4e74807bf034acff838fccda2ea5eee58
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2023-01-25 07:29:54 +0530

    Footer Component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,5 +1,55 @@
+import "./Footer.scss"
+
 export default function Footer() {
     return (
-        <h1>Footer</h1>
+        <div className="footer">
+            <div className="top">
+                <div className="item">
+                    <h2>Categories</h2>
+                    <span>Women</span>
+                    <span>Men</span>
+                    <span>Shoes</span>
+                    <span>Accessories</span>
+                    <span>New Arrivals</span>
+                </div>
+                <div className="item">
+                    <h2>Link</h2>
+                    <span>FAO</span>
+                    <span>Pages</span>
+                    <span>Stores</span>
+                    <span>Compare</span>
+                    <span>Cookies</span>
+                </div>
+                <div className="item">
+                    <h2>About</h2>
+                    <span>
+                        Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
+                        amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
+                        ut labore etdolore.
+                    </span>
+                </div>
+                <div className="item">
+                    <h2>Contact</h2>
+                    <span>
+                        Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
+                        amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
+                        ut labore etdolore.
+                    </span>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="left">
+                    <span className="logo">Urban Oasis</span>
+                    <span className="copyright">
+                        © Copyright 2023. All Rights Reserved
+                    </span>
+                </div>
+                <div className="right">
+                    <img src="/images/payment.png" alt="" />
+                </div>
+            </div>
+        </div>
     )
 }
